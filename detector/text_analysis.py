71b2a98f47a5516b119e5604d6523f1588f27e3b
Bhumika soor <124863109+Bhumikasoor@users.noreply.github.com>
2025-09-14 22:48:45 +0530

    Update text_analysis.py

--- detector/text_analysis.py
+++ detector/text_analysis.py
@@ -1,6 +1,12 @@
 import re
+import nltk
 from nltk.sentiment import SentimentIntensityAnalyzer
 
+try:
+    nltk.data.find("sentiment/vader_lexicon.zip")
+except LookupError:
+    nltk.download("vader_lexicon")
+    
 # Initialize sentiment analyzer
 sia = SentimentIntensityAnalyzer()
 
